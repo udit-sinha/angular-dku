6f9e1a2c66fca874863d3250f5d83d74d6e440a1
udit <udit.sinha@github.com>
2025-05-21 18:56:15 +1000

    api call

--- angular15-demo/src/app/app.component.ts
+++ angular15-demo/src/app/app.component.ts
@@ -1,10 +1,57 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular15-demo';
+  data: any;
+  metadata: any;
+  isLoading = true;
+  error: string | null = null;
+
+  constructor(private http: HttpClient) {}
+
+  ngOnInit(): void {
+    // Load data when component initializes
+    this.loadData();
+    this.loadMetadata();
+  }
+
+  loadData(): void {
+    this.getData().subscribe({
+      next: (response) => {
+        this.data = response;
+        this.isLoading = false;
+      },
+      error: (err) => {
+        this.error = 'Failed to load data: ' + err.message;
+        this.isLoading = false;
+        console.error('Error loading data:', err);
+      }
+    });
+  }
+
+  loadMetadata(): void {
+    this.getMetadata().subscribe({
+      next: (response) => {
+        this.metadata = response;
+      },
+      error: (err) => {
+        console.error('Error loading metadata:', err);
+      }
+    });
+  }
+
+  getData(): Observable<any> {
+    return this.http.get('/api/get_data');
+  }
+
+  getMetadata(): Observable<any> {
+    return this.http.get('/api/get_metadata');
+  }
 }
